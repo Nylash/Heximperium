ff1969622900d8299257501844da25bfd7130175
Nylash <ben.peltier7@gmail.com>
2025-03-31 17:00:35 +0200

    Turn and phase logic

--- HeximperiumProject/Assets/Scripts/Managers/GameManager.cs
+++ HeximperiumProject/Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,36 @@
 public class GameManager : Singleton<GameManager>
 {
+    private Phase _currentPhase;
+    private int _turnCounter;
 
+    public Phase CurrentPhase { get => _currentPhase; set => _currentPhase = value; }
+    public int TurnCounter { get => _turnCounter; set => _turnCounter = value; }
+
+    private void Start()
+    {
+        //Replace it by save logic
+        if (_currentPhase != Phase.Explore)
+        {
+            _currentPhase = Phase.Explore;
+        }
+    }
+
+    public void ConfirmPhase()
+    {
+        if (_currentPhase != Phase.Entertain) 
+        {
+            _currentPhase++;
+        }
+        else
+        {
+            _currentPhase = Phase.Explore;
+            _turnCounter++;
+        }
+        
+    }
+}
+
+public enum Phase
+{
+    Explore, Expand, Exploit, Entertain
 }
