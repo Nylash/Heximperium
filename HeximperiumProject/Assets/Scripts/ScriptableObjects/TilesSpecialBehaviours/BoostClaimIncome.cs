4da29feec10124ef842d5c38b17748c2e989a4fe
Nylash <ben.peltier7@gmail.com>
2025-06-21 17:35:11 +0200

    Territorial town

--- HeximperiumProject/Assets/Scripts/ScriptableObjects/TilesSpecialBehaviours/BoostClaimIncome.cs
+++ HeximperiumProject/Assets/Scripts/ScriptableObjects/TilesSpecialBehaviours/BoostClaimIncome.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Objects/Special Behaviour/BoostClaimIncome")]
 public class BoostClaimIncome : SpecialBehaviour
 {
+    [SerializeField] private int _claimQuantity;
+
     public override void InitializeSpecialBehaviour(Tile behaviourTile)
     {
-        ExpansionManager.Instance.ClaimPerTurn++;
+        for (int i = 0; i < _claimQuantity; i++)
+            ExpansionManager.Instance.ClaimPerTurn++;
     }
 
     public override void RollbackSpecialBehaviour(Tile behaviourTile)
     {
-        ExpansionManager.Instance.ClaimPerTurn--;
+        for (int i = 0; i < _claimQuantity; i++)
+            ExpansionManager.Instance.ClaimPerTurn--;
     }
 
     public override void HighlightImpactedTile(Tile behaviourTile, bool show)
