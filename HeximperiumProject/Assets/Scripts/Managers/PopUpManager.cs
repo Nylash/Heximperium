999880370bb5ca13a1557ff793c95a54a7b06517
Nylash <ben.peltier7@gmail.com>
2025-07-29 15:14:41 +0200

    Destroy popups

--- HeximperiumProject/Assets/Scripts/Managers/PopUpManager.cs
+++ HeximperiumProject/Assets/Scripts/Managers/PopUpManager.cs
@@ -103,7 +103,7 @@ public class PopUpManager : Singleton<PopUpManager>
         {
             foreach (GameObject item in _popUps)
             {
-                //Destrou popup
+                    Destroy(item);
             }
             _popUps.Clear();
         }
