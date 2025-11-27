f9fbaea9aa2cab406c6c6c1479e913c41a508c82
Nylash <ben.peltier7@gmail.com>
2025-11-27 21:36:13 +0100

    Use UI shader to avoid error in log

--- HeximperiumProject/Assets/Scripts/Managers/PopUpManager.cs
+++ HeximperiumProject/Assets/Scripts/Managers/PopUpManager.cs
@@ -683,7 +683,7 @@ public class PopUpManager : Singleton<PopUpManager>
 
         #region DETAIL
         TextMeshProUGUI detail = Instantiate(_text, popUp.transform.GetChild(1).transform).GetComponent<TextMeshProUGUI>();
-        detail.text = "<sprite name=\"Carnivalist_Emoji\"> are used during the Grand Jubilee to place " + Family.Entertainment.ToCustomString(true);
+        detail.text = "Carnivalists<sprite name=\"Carnivalist_Emoji\"> are used during the Grand Jubilee to place " + Family.Entertainment.ToCustomString(true);
         ClampTextWidth(detail);
         detail.alignment = TextAlignmentOptions.Center;
         detail.fontStyle = FontStyles.Italic;
