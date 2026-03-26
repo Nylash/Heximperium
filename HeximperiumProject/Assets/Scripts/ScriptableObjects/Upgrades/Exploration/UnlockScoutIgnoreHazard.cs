fe923939cb550851293a67e6c1990711ff89542f
Nylash <ben.peltier7@gmail.com>
2026-03-26 18:20:34 +0100

    Buff Trailblazer's Training (now Pathfinger Gear)

--- HeximperiumProject/Assets/Scripts/ScriptableObjects/Upgrades/Exploration/UnlockScoutIgnoreHazard.cs
+++ HeximperiumProject/Assets/Scripts/ScriptableObjects/Upgrades/Exploration/UnlockScoutIgnoreHazard.cs
@@ -6,10 +6,11 @@ public class UnlockScoutIgnoreHazard : UpgradeEffect
     public override void ApplyEffect()
     {
         ExplorationManager.Instance.UpgradeScoutIgnoreHazard = true;
+        ExplorationManager.Instance.BoostScoutRevealRadius += 1;
     }
 
     public override string GetEffectDescription()
     {
-        return "Scouts<sprite name=\"Scout_Emoji\"> ignore slow penalty from Mountain, Desert, Swamp and Water tiles";
+        return "Boosts Scouts'<sprite name=\"Scout_Emoji\"> reveal radius by 1 and they now ignore slow penalty from Mountain, Desert, Swamp and Water tiles";
     }
 }
