3bd3f36721b7eeec3f2de1b6361ca0e616d16fe3
Nylash <ben.peltier7@gmail.com>
2026-04-01 18:01:17 +0200

    Avoid dark hill when destroying infra on a basic tile hill

--- HeximperiumProject/Assets/Scripts/Objects/TintForUnclaimedSprite.cs
+++ HeximperiumProject/Assets/Scripts/Objects/TintForUnclaimedSprite.cs
@@ -5,12 +5,26 @@ public class TintForUnclaimedSprite : MonoBehaviour
     [SerializeField] private Color unclaimedTint = new Color(0.6862745f, 0.6666667f, 0.7254902f);
 
     private SpriteRenderer spriteRenderer;
+    private Tile tile;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = unclaimedTint;
+        tile = GetComponentInParent<Tile>();
 
-        transform.GetComponentInParent<Tile>().OnTileClaimed += (tile) => spriteRenderer.color = Color.white;
+        if (!tile.Claimed)
+        {
+            spriteRenderer.color = unclaimedTint;
+
+            tile.OnTileClaimed += (tile) => spriteRenderer.color = Color.white;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (tile != null)
+        {
+            tile.OnTileClaimed -= (tile) => spriteRenderer.color = Color.white;
+        }
     }
 }
